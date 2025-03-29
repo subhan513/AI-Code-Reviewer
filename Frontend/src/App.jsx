9b7142efec2c79f31898acdaa2d73bf7bf938ef2
subhan513 <subhancs57@gmail.com>
2025-03-29 16:24:01 +0500

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -25,7 +25,7 @@ async function ReviewCode() {
 
   return (
     <>
-    <h1 className='head'>AI code reviewer</h1>
+    <h1 className='head'>AI Code Reviewer</h1>
 <main>
         <div className="left">
           <div className="code">
